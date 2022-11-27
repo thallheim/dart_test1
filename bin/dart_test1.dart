b4ee8419857420c2074fe0005b934034a5d3878d
thallheim <orobinhedlund@gmail.com>
2022-11-27 05:19:02 +0100

    init

---
+++ bin/dart_test1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_test1/dart_test1.dart' as dart_test1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_test1.calculate()}!');
+}
---
+++ test/dart_test1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_test1/dart_test1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
