bbb80fa5e6eff397167ff127ec56f2e48a1af7d6
thallheim <robinhedlund@gmail.com>
2022-11-27 05:44:53 +0100

    import

--- bin/dart_test1.dart
+++ bin/dart_test1.dart
@@ -1,5 +1,9 @@
 import 'package:dart_test1/dart_test1.dart' as dart_test1;
+import 'dart:math';
+
+var r1 = Random().nextInt(10);
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_test1.calculate()}!');
+  print('Random 0-10: ${r1}');
 }
